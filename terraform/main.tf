68b8d57385f1663f22ee3125ea197cb2cb3d40b9
santo <faustinbrutosanto@gmail.com>
2025-05-30 04:22:00 +0200

    templatepacker + terraform ok

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,112 +2,96 @@ terraform {
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
-      version = "= 2.9.6"
+      version = "~> 2.9.6"
     }
   }
 }
 
-# Configuration du provider Proxmox
 provider "proxmox" {
   pm_api_url      = var.proxmox_api_url
-  pm_api_token_id = var.proxmox_api_token_id
-  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_user         = var.proxmox_user
+  pm_password     = var.proxmox_password
   pm_tls_insecure = true
 }
 
-# Configuration commune pour tous les VMs
 locals {
-  vm_settings = {
-    cores      = 2          # Nombre de cores CPU
-    memory     = 2048       # Mémoire RAM en MB
-    disk_size  = "15G"      # Taille du disque
-    target_node = "santoLAB" # Nom du node Proxmox
+  vm_base_config = {
+    target_node = var.target_node
+    clone       = var.template_name
+    full_clone  = true
+    os_type     = "cloud-init"
+    cores       = var.vm_defaults.cores
+    memory      = var.vm_defaults.memory
+    ciuser      = "ubuntu"
+    cipassword  = "admin"
+    sshkeys     = file("${path.module}/${var.ssh_public_key_file}")
+    boot        = "order=scsi0"
+    scsihw      = "virtio-scsi-single"
+    agent       = 1
   }
 }
 
 # --------- VM MASTER ---------
 resource "proxmox_vm_qemu" "k3s-master" {
-  vmid        = 108
-  name        = "k3s-master"
-  target_node = var.vm_settings.target_node
-  clone       = var.vm_settings.template
-  full_clone  = true
+  name     = "k3s-master"
+  vmid     = 108
 
-  # Configuration système
-  os_type = "cloud-init"
-  cores   = var.vm_settings.cores
-  memory  = var.vm_settings.memory
+  # Fusion avec la configuration de base
+  target_node = local.vm_base_config.target_node
+  clone       = local.vm_base_config.clone
+  full_clone  = local.vm_base_config.full_clone
+  os_type     = local.vm_base_config.os_type
+  cores       = local.vm_base_config.cores
+  memory      = local.vm_base_config.memory
+  sshkeys     = local.vm_base_config.sshkeys
+  ciuser      = local.vm_base_config.ciuser
+  cipassword  = local.vm_base_config.cipassword
+  boot        = local.vm_base_config.boot
+  scsihw      = local.vm_base_config.scsihw
+  agent       = local.vm_base_config.agent
 
-  # Configuration réseau
-  ipconfig0 = "ip=${var.network_config.master_ip}/24,gw=${var.network_config.gateway}"
-  sshkeys   = file("${path.module}/id_rsa.pub")
+  ipconfig0 = "ip=${var.network_config.base_ip}.${var.vm_ips["k3s-master"]}/${var.network_config.subnet},gw=${var.network_config.gateway}"
 
   network {
     model  = "virtio"
     bridge = "vmbr0"
   }
 
-  # Configuration du disque
   disk {
     slot     = 0
-    size     = var.vm_settings.disk_size
+    size     = var.vm_defaults.disk_size
     type     = "scsi"
     storage  = "local-lvm"
     iothread = 1
   }
 
-  boot    = "order=scsi0"
-  scsihw  = "virtio-scsi-single"
-  agent   = 1
+  timeouts {
+    create = "20m"
+    delete = "20m"
+  }
 }
 
-# --------- VM WORKER 1 ---------
-resource "proxmox_vm_qemu" "k3s-worker-1" {
-  vmid        = 109
-  name        = "k3s-worker-1"
-  target_node = var.vm_settings.target_node
-  clone       = var.vm_settings.template
-  full_clone  = true
+# --------- VM WORKERS ---------
+resource "proxmox_vm_qemu" "k3s-workers" {
+  count    = 2
+  name     = "k3s-worker-${count.index + 1}"
+  vmid     = 109 + count.index
 
-  os_type = "cloud-init"
-  cores   = var.vm_settings.cores
-  memory  = var.vm_settings.memory
+  # Fusion avec la configuration de base
+  target_node = local.vm_base_config.target_node
+  clone       = local.vm_base_config.clone
+  full_clone  = local.vm_base_config.full_clone
+  os_type     = local.vm_base_config.os_type
+  cores       = local.vm_base_config.cores
+  memory      = local.vm_base_config.memory
+  sshkeys     = local.vm_base_config.sshkeys
+  ciuser      = local.vm_base_config.ciuser
+  cipassword  = local.vm_base_config.cipassword
+  boot        = local.vm_base_config.boot
+  scsihw      = local.vm_base_config.scsihw
+  agent       = local.vm_base_config.agent
 
-  ipconfig0 = "ip=${var.network_config.worker1_ip}/24,gw=${var.network_config.gateway}"
-  sshkeys   = file("${path.module}/id_rsa.pub")
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  disk {
-    slot     = 0
-    size     = var.vm_settings.disk_size
-    type     = "scsi"
-    storage  = "local-lvm"
-    iothread = 1
-  }
-
-  boot    = "order=scsi0"
-  scsihw  = "virtio-scsi-single"
-  agent   = 1
-}
-
-# --------- VM WORKER 2 ---------
-resource "proxmox_vm_qemu" "k3s-worker-2" {
-  vmid        = 110
-  name        = "k3s-worker-2"
-  target_node = local.vm_settings.target_node
-  clone       = "packer-ubuntu"
-  full_clone  = true
-
-  os_type = "cloud-init"
-  cores   = local.vm_settings.cores
-  memory  = local.vm_settings.memory
-
-  ipconfig0 = "ip=192.168.1.111/24,gw=192.168.1.1"
-  sshkeys   = file("${path.module}/id_rsa.pub")
+  ipconfig0 = "ip=${var.network_config.base_ip}.${var.vm_ips["k3s-worker-${count.index + 1}"]}/${var.network_config.subnet},gw=${var.network_config.gateway}"
 
   network {
     model  = "virtio"
@@ -116,13 +100,14 @@ resource "proxmox_vm_qemu" "k3s-worker-2" {
 
   disk {
     slot     = 0
-    size     = local.vm_settings.disk_size
+    size     = var.vm_defaults.disk_size
     type     = "scsi"
     storage  = "local-lvm"
     iothread = 1
   }
 
-  boot    = "order=scsi0"
-  scsihw  = "virtio-scsi-single"
-  agent   = 1
+  timeouts {
+    create = "20m"
+    delete = "20m"
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,73 @@
- 
+variable "proxmox_api_url" {
+  description = "URL de l'API Proxmox"
+  type        = string
+  default     = "https://192.168.1.4:8006/api2/json"
+}
+
+variable "proxmox_user" {
+  description = "Utilisateur Proxmox"
+  type        = string
+  default     = "root@pam"
+}
+
+variable "proxmox_password" {
+  description = "Mot de passe Proxmox"
+  type        = string
+  sensitive   = true
+}
+
+variable "template_name" {
+  description = "Nom du template à utiliser"
+  type        = string
+  default     = "packer-ubuntu"
+}
+
+variable "target_node" {
+  description = "Node Proxmox cible"
+  type        = string
+  default     = "santoLAB"
+}
+
+variable "vm_defaults" {
+  description = "Configurations par défaut des VMs"
+  type = object({
+    cores   = number
+    memory  = number
+    disk_size = string
+  })
+  default = {
+    cores     = 2
+    memory    = 2048
+    disk_size = "15G"
+  }
+}
+
+variable "network_config" {
+  description = "Configuration réseau"
+  type = object({
+    gateway = string
+    subnet  = string
+    base_ip = string
+  })
+  default = {
+    gateway = "192.168.1.1"
+    subnet  = "24"
+    base_ip = "192.168.1"
+  }
+}
+
+variable "vm_ips" {
+  description = "IPs des VMs"
+  type = map(string)
+  default = {
+    "k3s-master"   = "18"
+    "k3s-worker-1" = "19"
+    "k3s-worker-2" = "20"
+  }
+}
+
+variable "ssh_public_key_file" {
+  description = "Chemin vers la clé SSH publique"
+  type        = string
+  default     = "id_rsa.pub"
+} 
